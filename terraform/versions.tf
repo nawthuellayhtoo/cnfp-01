a2d007dec779c08b07d7801d59f289741665ee41
Naw Thue Llay Htoo <nawthuellayhtoo@gmail.com>
2024-09-14 11:21:05 +0800

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+}
 
+provider "aws" {
+  shared_config_files      = ["/Users/nawthuellayhtoo/.aws/config"]
+  shared_credentials_files = ["/Users/nawthuellayhtoo/.aws/credentials"]
+  profile                  = "ntlh-dynamo-role"
+}
